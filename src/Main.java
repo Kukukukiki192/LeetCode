39c48c1943624ec6f25074c1d48f38916075be94
Kukukukiki192 <60028604+Kukukukiki192@users.noreply.github.com>
2024-06-10 10:28:47 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,39 @@ import java.util.*;
  * @author K
  */
 public class Main {
+    public class Node {
+        int val;
+        Node next;
+
+        Node() {
+        }
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        Node head = new Node();
         while (n-- > 0) {
-            String str = in.next();
-            StringBuilder s = new StringBuilder();
-            for (int i = 0; i < str.length(); i += 2) {
-                s.append(str.charAt(i + 1));
-                s.append(str.charAt(i));
-            }
-            System.out.println(s);
+            int i = in.nextInt();
+            //初始化链表
         }
+        int m = in.nextInt();
+        in.nextLine();//读取\n 将cursor放到下一行
+        while (m-- > 0) {
+            String line = in.nextLine();
+            String[] str = line.split(" ");
+
+        }
+        System.out.println();
+
     }
 }
